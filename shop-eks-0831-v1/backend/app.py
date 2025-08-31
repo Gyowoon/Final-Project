a9eda8ccafb00c51486968d908d35bffbb844dc4
Gyowoon <158446209+Gyowoon@users.noreply.github.com>
2025-08-31 21:55:29 +0900

    0831v2

--- shop-eks-0831-v1/backend/app.py
+++ shop-eks-0831-v1/backend/app.py
@@ -50,7 +50,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False) # Length Limit Changed @08/31
     name = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.String(20))
     points_balance = db.Column(db.Integer, default=0, nullable=False)
